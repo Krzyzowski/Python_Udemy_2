832b85007588c5886da871b5efab6b46fcc1b212
Krzyzowski <128824402+Krzyzowski@users.noreply.github.com>
2025-03-02 21:49:08 +0100

    Add files via upload

---
+++ 2_31 bool conversion.py
@@ -0,0 +1,27 @@
+
+print(bool()) # False
+
+# falsy values
+
+print("# falsy values")
+print( )
+
+print( bool(False)) #False
+print( bool(0)) #False
+print( bool(0.0)) #False
+print( bool( () )) #False
+print( bool( [] )) #False
+print( bool( {} )) #False
+print( bool( "" )) #False
+print( bool( None )) #False
+print( )
+
+print("True values")
+print( bool(True ))
+print(bool(10))
+print(bool(-10))
+print(bool(10.3333333333))
+print(bool(1,2,3))
+
+
+
