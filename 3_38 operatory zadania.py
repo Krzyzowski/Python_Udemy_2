9264a8287cd4b030f8096fbbf57fb3cbe1840890
Krzyzowski <128824402+Krzyzowski@users.noreply.github.com>
2025-03-14 08:59:44 +0100

    Add files via upload

---
+++ 3_38 operatory zadania.py
@@ -0,0 +1,24 @@
+
+balance = 1000
+
+balance += 7000
+print("balance += 7000:",balance)
+print( )
+
+balance -= 2000
+print("balance -= 2000 :", balance)
+print( )
+
+balance *=3
+print("balance *= 3 :",balance)
+print
+
+balance -= 4000
+print("balance -= 4000:", balance)
+
+
+balance += 4000
+balance /= 3
+balance -= 4000
+
+print(balance)
---
+++ 3_40 zadania.py
@@ -0,0 +1,21 @@
+
+print("15 > 10:", 15 > 10)
+
+print("5 = 5", 5 == 5)
+
+x = 20
+y = 30
+
+print(" x > y", x > y)
+
+a = 100
+b = 101
+print("100 => 101",  a >= b)
+
+if 50 != 20:
+    print(" 50 nie równe 20")
+
+if "hello" != "goodbye":
+    print("hello != goodbye")
+
+
